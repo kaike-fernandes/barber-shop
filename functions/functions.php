84323175d43302917dfeb89be82589a849842cf8
kaike.fernandes <kaike.fernandes@meuinc.com.br>
2025-01-09 15:02:40 -0300

    teste nova branch

---
+++ functions/functions.php
@@ -0,0 +1,8 @@
+<?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+date_default_timezone_set('America/Sao_Paulo');
+
+?>
