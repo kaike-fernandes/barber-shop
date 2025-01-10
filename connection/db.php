7449687d36da7e1c5e343bd50ce4e732d3cd5d05
kaike.fernandes <kaike.fernandes@meuinc.com.br>
2025-01-10 16:49:00 -0300

    conexão com banco de dados

---
+++ connection/db.php
@@ -0,0 +1,34 @@
+<?php 
+// include composer
+require '../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+// pegar variaveis de ambiente
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+if (!file_exists('../.env')) {
+    die('Arquivo .env não encontrado!');
+} else {
+    $hostname = $_ENV['HOSTNAME'];
+    $db = $_ENV['DATABASE'];
+    $user = $_ENV['USER'];
+    $pass = $_ENV['PASS'];
+    $charset = 'utf8mb4';
+}
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$dsn = "mysql:host=$hostname;dbname=$db;chaset=$charset";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $err) {
+    throw new PDOException($err->getMessage(), (int)$e->getCode());
+}
+
+?>
